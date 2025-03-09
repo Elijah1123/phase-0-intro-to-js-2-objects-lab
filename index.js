bdb000db2b473c6316d217f7226bd02a21bdb6de
Elijah <elijah.macharia@student.moringaschool.com>
2025-03-09 23:18:36 +0300

    arrays

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+// Define an employee object
+const employee = {
+    name: "Sam",
+  };
+  
+  // Non-destructive update: Returns a new object without modifying the original
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return { ...employee, [key]: value };
+  }
+  
+  // Destructive update: Modifies the original object
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  // Non-destructive delete: Returns a new object without modifying the original
+  function deleteFromEmployeeByKey(employee, key) {
+    let newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  // Destructive delete: Modifies the original object
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+  
